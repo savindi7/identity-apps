8c940b1d335dc3f45ba5f8959b3bfa4fec9c6052
RavindiFernando <ravindifernando3@gmail.com>
2026-03-27 11:06:53 +0530

    Reapply "Fix type errors and bump SDK version"
    This reverts commit 7d6c48f32cfc892cf94438d6e030964810dca3c6.

--- features/admin.tenants.v1/api/get-tenant-domain-availability.ts
+++ features/admin.tenants.v1/api/get-tenant-domain-availability.ts
@@ -20,7 +20,9 @@ import { AsgardeoSPAClient, HttpClientInstance } from "@asgardeo/auth-react";
 import { store } from "@wso2is/admin.core.v1/store";
 import { RequestConfigInterface } from "@wso2is/admin.core.v1/hooks/use-request";
 import { IdentityAppsApiException } from "@wso2is/core/exceptions";
-import { HttpMethods } from "@wso2is/core/models";
+import { HttpMethods,
+    HttpErrorResponseDataInterface
+} from "@wso2is/core/models";
 import { AxiosError } from "axios";
 import TenantConstants from "../constants/tenant-constants";
 
