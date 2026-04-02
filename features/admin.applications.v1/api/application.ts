579b74d42dfcf9911b46a601938d1a22950cfbca
AmshikaH <amshikahameed@gmail.com>
2026-04-02 10:00:37 +0530

    Update well-known endpoint in app info

--- features/admin.applications.v1/api/application.ts
+++ features/admin.applications.v1/api/application.ts
@@ -1100,7 +1100,8 @@ export const getOIDCApplicationConfigurations = (): Promise<OIDCApplicationConfi
                 tokenRevocationEndpoint: response.data.revocation_endpoint,
                 userEndpoint: response.data.userinfo_endpoint,
                 webFingerEndpoint: response.data.webfinger_endpoint,
-                wellKnownEndpoint: `${ response.data.token_endpoint }/.well-known/openid-configuration`
+                wellKnownEndpoint: !state.config.deployment.tenantContext.enableTenantQualifiedUrls ?
+                    requestConfig.url : `${ response.data.token_endpoint }/.well-known/openid-configuration`
             };
 
             return Promise.resolve(oidcConfigs);
