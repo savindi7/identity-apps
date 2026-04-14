3790c522baaecf92343958eea36c59e44b304189
Pavindu Lakshan <pavindulakshan@gmail.com>
2026-04-14 21:22:52 +0530

    Address lint errors

--- modules/react-components/.eslintrc.js
+++ modules/react-components/.eslintrc.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ * Copyright (c) 2020-2026, WSO2 LLC. (https://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -17,7 +17,10 @@
  */
 
 module.exports = {
-    extends: ["../../.eslintrc.js", "plugin:storybook/recommended"],
+    extends: [
+        "../../.eslintrc.js",
+        "plugin:storybook/recommended"
+    ],
     rules: {
         "@typescript-eslint/typedef": "off"
     }
