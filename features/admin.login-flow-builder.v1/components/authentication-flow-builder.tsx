3fbe775e92755bf420c36be432887f04c83de68d
Ashan Thamara <ashanthamara98@gmail.com>
2026-04-23 01:15:20 +0530

    Update import

--- features/admin.login-flow-builder.v1/components/authentication-flow-builder.tsx
+++ features/admin.login-flow-builder.v1/components/authentication-flow-builder.tsx
@@ -40,9 +40,7 @@ import useFeatureGate, { UseFeatureGateInterface } from "@wso2is/admin.feature-g
 import useAILoginFlow from "@wso2is/admin.login-flow.ai.v1/hooks/use-ai-login-flow";
 import { OrganizationType } from "@wso2is/admin.organizations.v1/constants/organization-constants";
 import { isFeatureEnabled } from "@wso2is/core/helpers";
-import { AlertLevels, IdentifiableComponentInterface,
-    HttpErrorResponseDataInterface
-} from "@wso2is/core/models";
+import { AlertLevels, HttpErrorResponseDataInterface, IdentifiableComponentInterface } from "@wso2is/core/models";
 import { addAlert } from "@wso2is/core/store";
 import { PrimaryButton } from "@wso2is/react-components";
 import { AxiosError } from "axios";
