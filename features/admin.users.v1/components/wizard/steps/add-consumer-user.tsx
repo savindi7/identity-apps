d83e4c0c7d9e7b49fe24fc7382bdd171c14b0232
Pavindu Lakshan <pavindulakshan@gmail.com>
2026-05-01 13:20:53 +0530

    Fix self referencing

--- features/admin.users.v1/components/wizard/steps/add-consumer-user.tsx
+++ features/admin.users.v1/components/wizard/steps/add-consumer-user.tsx
@@ -19,9 +19,9 @@
 import { AppState } from "@wso2is/admin.core.v1/store";
 import { SharedUserStoreUtils } from "@wso2is/admin.core.v1/utils/user-store-utils";
 import { userstoresConfig } from "@wso2is/admin.extensions.v1/configs/userstores";
-import { getUsersList } from "@wso2is/admin.users.v1/api/users";
-import { UserListInterface } from "@wso2is/admin.users.v1/models/user";
-import { getConfiguration, getUsernameConfiguration } from "@wso2is/admin.users.v1/utils";
+import { getUsersList } from "../../../api/users";
+import { UserListInterface } from "../../../models/user";
+import { getConfiguration, getUsernameConfiguration } from "../../../utils";
 import { useUserStoreRegEx } from "@wso2is/admin.userstores.v1/api/use-get-user-store-regex";
 import { USERSTORE_REGEX_PROPERTIES } from "@wso2is/admin.userstores.v1/constants/user-store-constants";
 import { useValidationConfigData } from "@wso2is/admin.validation.v1/api";
@@ -48,7 +48,7 @@ export interface AddConsumerUserProps {
     initialValues: any;
     triggerSubmit: boolean;
     emailVerificationEnabled: boolean;
-    onSubmit: (values: any) => void;
+    onSubmit: (_values: any) => void;
     hiddenFields?: ("userName" | "firstName" | "lastName" | "password")[];
     requestedPasswordOption?: "ask-password" | "create-password";
 }
