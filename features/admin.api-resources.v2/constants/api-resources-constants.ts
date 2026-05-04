9f83cfe83d28d8582051f2d38b3315e291219fe6
Lakshan-Banneheke <btlakshan@gmail.com>
2026-05-04 17:18:07 +0530

    Add support for handling api resources tier limit reached

--- features/admin.api-resources.v2/constants/api-resources-constants.ts
+++ features/admin.api-resources.v2/constants/api-resources-constants.ts
@@ -17,6 +17,7 @@
  */
 
 import { AppConstants } from "@wso2is/admin.core.v1/constants/app-constants";
+import { IdentityAppsError } from "@wso2is/core/errors";
 
 /**
  * Class containing API resources constants.
@@ -48,6 +49,16 @@ export class APIResourcesConstants {
     // API Resource required attributes
     public static readonly PROPERTIES: string = "properties";
     public static readonly IS_CHOREO_API: string = "isChoreoAPI";
+
+    /**
+     * API resource create limit reached error.
+     */
+    public static readonly ERROR_CREATE_LIMIT_REACHED: IdentityAppsError = new IdentityAppsError(
+        "ASG-FG-API-60002",
+        "apiResources:notifications.apiLimitReachedError.error.description",
+        "apiResources:notifications.apiLimitReachedError.error.message",
+        "f3d8c4a2-1e7b-4d92-8c3a-9b6e2d4f8a51"
+    );
 
     /**
      * Get the API resource paths as a map.
